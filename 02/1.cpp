69efec64287a1f8ea19b98deda96a313e89f8018
lhall2 <lhall2@fbi.gov>
2019-12-02 11:06:30 -0500

    Day 2 done

--- 02/1.cpp
+++ 02/1.cpp
@@ -5,6 +5,28 @@
 #include <string>
 #include <vector>
 
+int getIntCodeOutput(std::vector<int> input) {
+    int i = 0;
+    std::vector<int> parsedCsv = input;
+    while(i < parsedCsv.size()){
+        if(parsedCsv[i] == 1){
+            parsedCsv[parsedCsv[i+3]] = parsedCsv[parsedCsv[i+1]] + parsedCsv[parsedCsv[i+2]];
+            i += 4;
+        } else if (parsedCsv[i] == 2){
+            parsedCsv[parsedCsv[i+3]] = parsedCsv[parsedCsv[i+1]] * parsedCsv[parsedCsv[i+2]];
+            i += 4;
+        } else if (parsedCsv[i] == 99){
+            break;
+        } else {
+            std::cout << "Something went wrong!" << std::endl;
+            break;
+            
+        }
+    }
+
+    return parsedCsv[0];
+
+}
 
 int main() {
 
@@ -16,34 +38,31 @@ int main() {
         std::stringstream lineStream(str);
         std::string cell;
         while(std::getline(lineStream,cell,',')) {
-            parsedCsv.push_back(std::stoi(cell));
+            std::cout << cell << std::endl;
+            std::cout << std::stoi(cell) << std::endl;
+            int cellVal = std::stoi(cell);
+            parsedCsv.push_back(cellVal);
         }
 
     }
-
     
-
-    parsedCsv[1] = 12;
-    parsedCsv[2] = 2;
-
-    for(int i = 0; i < parsedCsv.size(); i++){
-        std::cout << "parsedCsv[i]: " << parsedCsv[i] << std::endl;
-        std::cout << "parsedCsv[i+3]: " << parsedCsv[i+3] << std::endl;
-        std::cout << "parsedCsv[parsedCsv[i+3]]: " << parsedCsv[parsedCsv[i+3]] << std::endl;
-        if(parsedCsv[i] == 1){
-            parsedCsv[parsedCsv[i+3]] = parsedCsv[i+1] + parsedCsv[i+2];
-            i += 4;
-        } else if (parsedCsv[i] == 2){
-            parsedCsv[parsedCsv[i+3]] = parsedCsv[i+1] * parsedCsv[i+2];
-            i += 4;
-        } else if (parsedCsv[i] == 99){
-            break;
-        } else {
-            std::cout << "Something went wrong!" << std::endl;
-            break;
-            
+    for(int i=0; i<100; i++) {
+        int result = 0;
+        for(int j=0; j<100; j++) {
+             std::vector<int> inputTry = parsedCsv;
+             inputTry[1] = i;
+             inputTry[2] = j;
+             result = getIntCodeOutput(inputTry);
+             if(result == 19690720) {
+                std::cout << "Success! " << i << " " << j << std::endl;
+                break;
+             }
+        }
+        if(result == 19690720) {
+                break;
         }
     }
-    
     std::cout << parsedCsv[0] << std::endl;
 }
+
+
