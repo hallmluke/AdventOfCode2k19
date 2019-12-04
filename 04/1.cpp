2f883b910c6c6eb46efbd2556387abf803edad76
Luke Hall <hallmluke@gmail.com>
2019-12-04 08:23:58 -0500

    Day 4 done

--- 04/1.cpp
+++ 04/1.cpp
@@ -4,7 +4,22 @@
 bool adjacentDigits(std::string password) {
     for(int i=0; i<password.length() - 1; i++) {
         if(password[i] == password[i+1]) {
-            return true;
+            if(i > 0) {
+                if (i+2 >= password.length()) {
+                    if(password[i-1] != password[i]) {
+                        return true;
+                    }
+                } else {
+                    if(password[i-1] != password[i]  && password[i+1] != password[i+2]) {
+                        return true;
+                    }
+                }
+            } else {
+                if(password[i+1] != password[i+2]) {
+                    return true;
+                }
+            }
+            
         }
     }
     return false;
@@ -23,7 +38,8 @@ int main() {
     int lowerBound = 240920;
     int upperBound = 789857;
 
-    std::cout << adjacentDigits("123789") << increasingDigits("123789") << std::endl;
+    std::cout << adjacentDigits("123444") << increasingDigits("123789") << std::endl;
+    std::cout << adjacentDigits("111122") << increasingDigits("123789") << std::endl;
 
     int matches = 0;
     for(int i=lowerBound; i<=upperBound; i++) {
