1e7e578d563c008400811ddab900528105b8e330
Luke Hall <hallmluke@gmail.com>
2019-12-07 17:59:42 -0500

    Day 7 done, excellent

--- 07/1.cpp
+++ 07/1.cpp
@@ -20,20 +20,34 @@ void permute(std::string a, int l, int r) {
     }
 }
 
-int getIntCodeOutput(std::vector<int> input, int userInput, int phaseSetting)
+class Amp {
+    public:
+    Amp(std::vector<int> _numbers, int _position = 0, int _phase = 0, int _input = 0) {
+        numbers = _numbers;
+        position = _position;
+        phase = _phase;
+        input = _input;
+    }
+    std::vector<int> numbers;
+    int position;
+    int phase;
+    int input;
+};
+
+int getIntCodeOutput(std::vector<Amp> amplifiers)//, int userInput, int phaseSetting)
 {
     int i = 0;
-    std::vector<int> vals = input;
-    int programInput = userInput;
+    std::vector<int> vals = amplifiers[0].numbers;
+    int currentAmplifier = 0;
     int programOutput = 0;
 
     while (i < vals.size())
     {
         //std::string instruction = vals[i];
         int instructionVal = vals[i]; //std::stoi(instruction);
-        //std::cout << "Instruction: " << instructionVal << std::endl;
+        std::cout << "Instruction: " << instructionVal << std::endl;
         int opCode = instructionVal % 100;
-        //std::cout << "Op Code: " << opCode << std::endl;
+        std::cout << "Op Code: " << opCode << std::endl;
         std::vector<int> parameterModes;
 
         instructionVal = instructionVal / 100;
@@ -102,15 +116,17 @@ int getIntCodeOutput(std::vector<int> input, int userInput, int phaseSetting)
             i += parameters.size() + 2;
             break;
         case 3:
-            if(i == 0) {
-                vals[vals[i + 1]] = phaseSetting;
-            } else {
-                vals[vals[i+1]] = programInput;
-            }
+            //if(i == 0) {
+             //   vals[vals[i + 1]] = phaseSetting;
+            //} else {
+                vals[vals[i+1]] = amplifiers[currentAmplifier].phase;
+                amplifiers[currentAmplifier].phase = amplifiers[currentAmplifier].input;
+            //}
             i += 2;
             break;
         case 4:
             //programOutput = vals[vals[i+1]; // use params
+            std::cout << "Hit 4" << std::endl;
             if (parameters.size() > 0)
             {
                 programOutput = parameters[0];
@@ -119,12 +135,24 @@ int getIntCodeOutput(std::vector<int> input, int userInput, int phaseSetting)
             {
                 programOutput = vals[vals[i + 1]];
             }
-            if (vals[i + 2] != 99 && programOutput != 0)
+            /*if (vals[i + 2] != 99 && programOutput != 0)
             {
                 std::cout << "ugh" << std::endl;
                 return -1;
-            }
+            } */
             i += 2;
+            amplifiers[currentAmplifier].position = i;
+            amplifiers[currentAmplifier].numbers = vals;
+
+            currentAmplifier = (currentAmplifier + 1) % 5;
+            std::cout << "Current amplifier " << currentAmplifier << std::endl;
+            i = amplifiers[currentAmplifier].position;
+            vals = amplifiers[currentAmplifier].numbers;
+
+            if(amplifiers[currentAmplifier].phase == amplifiers[currentAmplifier].input) {
+                amplifiers[currentAmplifier].phase = programOutput;
+            }
+            amplifiers[currentAmplifier].input = programOutput;
             break;
         case 5:
             if (parameters[0] != 0)
@@ -235,25 +263,30 @@ int main()
     // params: 103, 90
     // int output = getIntCodeOutput(testVals2, 0);
 
-std::string phases = "01234";
+std::string phases = "56789";
 permute(phases, 0, 4);
 int maxThrust = 0;
 int ptr = 0;
+std::vector<Amp> amplifiers(5, parsedCsv);
+
 for(int i=0; i<permutations.size(); i++){
     std::string permVal = permutations[i];
     std::cout << "Perm val " << permVal << std::endl;
     int input = 0;
-    while(permVal.size() > 0) {
-        std::cout << "Perm val in loop" << permVal << std::endl;
-        int phase = permVal[0] - '0';
-        std::cout << "Phase " << phase << std::endl;
-        std::cout << "Input " << input << std::endl;
-        std::vector<int> ampProgram = parsedCsv;
+    //while(permVal.size() > 0) {
+    for(int j = 0; j < permVal.size(); j++) {
+        std::cout << "Perm val in loop" << permVal[j] << std::endl;
+        int phase = permVal[j] - '0';
         //std::cout << "Phase " << phase << std::endl;
-        input = getIntCodeOutput(ampProgram, input, phase);
-        std::cout << "Finished Amp" << std::endl;
-        permVal = permVal.substr(1);
+        //std::cout << "Input " << input << std::endl;
+        //std::vector<int> ampProgram = parsedCsv;
+        //std::cout << "Phase " << phase << std::endl;
+        amplifiers[j].phase = phase;
+        std::cout << "After phase assign" << std::endl;
+        
     }
+    input = getIntCodeOutput(amplifiers);
+    //std::cout << "Finished Amp" << std::endl;
     if(input > maxThrust) {
         maxThrust = input;
         ptr = i;
